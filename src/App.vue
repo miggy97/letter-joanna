ec635be51e7aeb1ff98c5d3e10da08cefbd72011
miggy97 <miguelingelmo@hotmail.com>
2021-07-12 19:39:04 +0200

    the real commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <LetterIcon @clicked="showLetter" />
+  <div class="sections">
+    <button>Intro</button>
+    <button>Studies</button>
+    <button>Work</button>
+    <button>Love</button>
+    <button>Health</button>
+  </div>
+  <transition name="moveup">
+    <Letter v-if="isIconClick" />
+  </transition>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import LetterIcon from "./components/LetterIcon.vue";
+import Letter from "./components/Letter.vue";
+import gsap from "gsap";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    LetterIcon,
+    Letter,
+  },
+  data() {
+    return {
+      isIconClick: false,
+      tl: null,
+    };
+  },
+  methods: {
+    showLetter() {
+      setTimeout(() => {
+        this.isIconClick = true;
+      }, 4000);
+      setTimeout(() => {
+        this.tl.resume();
+      }, 4800);
+    },
+  },
+  mounted() {
+    let tl = gsap.timeline();
+    tl.from(".sections button", {
+      x: -80,
+      opacity: 0,
+      stagger: 0.2,
+      duration: 1,
+      ease: "back",
+    });
+    tl.pause();
+    this.tl = tl;
+
   },
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Yomogi&display=swap");
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  overflow-x: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  padding-right: 3em;
+  padding-left: 11em;
+  padding-top: 2.5em;
+}
+
+.sections {
+  position: absolute;
+  left: 2.5em;
+  top: 13em;
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  button {
+    border-radius: 2rem;
+    padding: 0.5em 2em;
+    border: 1px solid #4484f4;
+    background: white;
+    font-family: "Yomogi", cursive;
+    font-size: 1em;
+    font-weight: 500;
+    color: black;
+    cursor: pointer;
+
+    &:nth-child(2) {
+      border: 1px solid #7ed448;
+    }
+    &:nth-child(3) {
+      border: 1px solid #f1ad3e;
+    }
+    &:nth-child(4) {
+      border: 1px solid #e962ac;
+    }
+    &:nth-child(5) {
+      border: 1px solid #ae49d6;
+    }
+  }
+}
+
+.moveup-enter-active {
+  transition: 0.8s;
+}
+.moveup-enter-from/* .fade-leave-active below version 2.1.8 */ {
+  transform: translate(0, 100%);
+  opacity: 0;
 }
 </style>
---
+++ src/components/Letter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="letter">
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Letter",
+};
+</script>
+
+<style lang="scss" scoped>
+.letter {
+  width: 50%;
+  height: 90vh;
+  border: 1px solid #4484f4;
+  border-radius: 5px;
+
+  @media (max-width: 1350px) {
+    width: 60%;
+  }
+  @media (max-width: 1150px) {
+    width: 75%;
+  }
+  @media (max-width: 950px) {
+    width: 85%;
+  }
+  @media (max-width: 750px) {
+    width: 95%;
+  }
+  @media (max-width: 550px) {
+    width: 100%;
+  }
+}
+</style>
